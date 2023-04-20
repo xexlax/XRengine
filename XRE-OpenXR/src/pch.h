0f4a041d18f30dd03a72e95f0f2059b64b8e8c47
xexlax <923358767@qq.com>
2023-04-20 17:55:58 +0800

    OpenXR OpenGL Demo

---
+++ XRE-OpenXR/src/pch.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2017-2023, The Khronos Group Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdarg>
+#include <cstdio>
+#include <exception>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <set>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <vector>
+
+#include <time.h>
+#include <string.h>
+#define XR_USE_PLATFORM_WIN32
+#define XR_USE_GRAPHICS_API_OPENGL
+//
+// Platform Headers
+//
+#ifdef XR_USE_PLATFORM_WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif  // !WIN32_LEAN_AND_MEAN
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif  // !NOMINMAX
+
+#include <windows.h>
+#include <wrl/client.h>  // For Microsoft::WRL::ComPtr
+#endif
+
+
+
+//
+// Graphics Headers
+//
+
+
+#ifdef XR_USE_GRAPHICS_API_OPENGL
+
+#ifdef XR_USE_PLATFORM_WIN32
+#include <wingdi.h>  // For HGLRC
+#include <Glad/glad.h>
+#endif
+#endif
+
+
+
+//
+// OpenXR Headers
+//
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+#include <openxr/openxr_reflection.h>
